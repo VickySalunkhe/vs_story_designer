b66a483ebc5f58344d4ca090339c70b974132f67
Vicky Salunkhe <salunkhevicky01@gmail.com>
2022-12-14 16:35:58 +0530

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter stories designer Demo',
+      title: 'Flutter story designer Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
