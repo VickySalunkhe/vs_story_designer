6bbfdb836f9b6a3f4bef8a86fc1343f812b95003
Madhav Gupta <madhav.gupta7674@gmail.com>
2025-05-13 23:48:59 +0530

    fex: add support for Flutter 3.29+

--- lib/src/domain/sevices/save_as_image.dart
+++ lib/src/domain/sevices/save_as_image.dart
@@ -3,8 +3,8 @@ import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:vision_gallery_saver/vision_gallery_saver.dart';
 
 Future takePicture(
     {required contentKey,
@@ -28,7 +28,7 @@ Future takePicture(
     await capturedFile.writeAsBytes(pngBytes);
 
     if (saveToGallery) {
-      final result = await ImageGallerySaver.saveImage(pngBytes,
+      final result = await VisionGallerySaver.saveImage(pngBytes,
           quality: 100, name: "${fileName}_${DateTime.now()}.png");
       if (result != null) {
         return true;
